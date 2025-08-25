787ca5c4e80cfe11030df1dd40736241c64c85ca
devdorong <devdorong@gmail.com>
2025-08-25 16:53:03 +0900

    [docs] 기본 환경설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 const Sample = (): JSX.Element => {
   return <div>샘플입니다.</div>;
 };
 
 const App = (): JSX.Element => {
+  // ts
+  const [count, setCount] = useState(0);
+
+  // tsx
   return (
     <div>
       <h1>App</h1>
       <Sample></Sample>
+      <div>Count : {count}</div>
+      <button onClick={() => setCount(count + 1)}>증가</button>
+      <button onClick={() => setCount(count - 1)}>감소</button>
+      <button onClick={() => setCount(0)}>리셋</button>
     </div>
   );
 };
