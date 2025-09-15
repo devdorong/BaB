69b283bb99d2a9c987e9ed1d8033bb17e620427b
dev.gsheep <dev.gsheep@gmail.com>
2025-09-15 17:09:49 +0900

    [docs] LoginPage 및 ReviewCard

--- src/components/member/MemberHeader.tsx
+++ src/components/member/MemberHeader.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ButtonFillMd, ButtonLineMd } from '../../ui/button';
 import { LogoSm } from '../../ui/Ui';
-import { Chat3Line } from '../../ui/Modal';
+import { Chat3Line, CustomerServiceLine, GiftLine, StarLine } from '../../ui/Modal';
 import { RiChat3Line } from 'react-icons/ri';
 
 const MemberHeader = () => {
@@ -12,21 +12,30 @@ const MemberHeader = () => {
           <LogoSm />
         </Link>
         <div className="flex items-center gap-[40px] ">
-          <div className="flex items-center gap-[40px]">
-            <Link to={'/member/community'}>
-              <Chat3Line color="none" bgColor="none" />
+          <div className="flex items-center gap-[40px] text-babgray-800">
+            <Link to={'/member/community'} className="flex items-center">
+              <Chat3Line color="none" bgColor="none" size={16} />
               커뮤니티
             </Link>
-            <Link to={'/member/reviews'}>맛집리뷰</Link>
-            <Link to={'/member/events'}>이벤트</Link>
-            <Link to={'/member/support'}>고객센터</Link>
+            <Link to={'/member/reviews'} className="flex items-center">
+              <StarLine color="none" bgColor="none" size={16} />
+              맛집리뷰
+            </Link>
+            <Link to={'/member/events'} className="flex items-center">
+              <GiftLine color="none" bgColor="none" size={16} />
+              이벤트
+            </Link>
+            <Link to={'/member/support'} className="flex items-center">
+              <CustomerServiceLine color="none" bgColor="none" size={16} />
+              고객센터
+            </Link>
           </div>
           <div className="flex gap-[10px]">
             <Link to={'/member/login'}>
-              <ButtonLineMd>로그인</ButtonLineMd>
+              <ButtonLineMd style={{ fontWeight: 400 }}>로그인</ButtonLineMd>
             </Link>
             <Link to={'/member/signup'}>
-              <ButtonFillMd>회원가입</ButtonFillMd>
+              <ButtonFillMd style={{ fontWeight: 400 }}>회원가입</ButtonFillMd>
             </Link>
           </div>
         </div>
