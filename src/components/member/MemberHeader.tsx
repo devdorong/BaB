8ebd24e73ab4d17d1e8a3ef1d905c07676d76b99
devdorong <devdorong@gmail.com>
2025-10-02 15:24:04 +0900

    [docs] 헤더 스타일 수정

--- src/components/member/MemberHeader.tsx
+++ src/components/member/MemberHeader.tsx
@@ -66,19 +66,19 @@ const MemberHeader = () => {
         </Link>
         <div className="flex items-center gap-[40px] ">
           <div className="flex items-center gap-[40px] text-babgray-800">
-            <Link to={'/member/community'} className="flex items-center gap-[16px]">
+            <Link to={'/member/community'} className="flex items-center gap-[16px] hover:text-bab">
               <Chat3Line color="none" bgColor="none" size={16} />
               <p>커뮤니티</p>
             </Link>
-            <Link to={'/member/reviews'} className="flex items-center gap-[16px]">
+            <Link to={'/member/reviews'} className="flex items-center gap-[16px] hover:text-bab">
               <StarLine color="none" bgColor="none" size={16} />
               <p>맛집추천</p>
             </Link>
-            <Link to={'/member/events'} className="flex items-center gap-[16px]">
+            <Link to={'/member/events'} className="flex items-center gap-[16px] hover:text-bab">
               <GiftLine color="none" bgColor="none" size={16} />
               <p>이벤트</p>
             </Link>
-            <Link to={'/member/support'} className="flex items-center gap-[16px]">
+            <Link to={'/member/support'} className="flex items-center gap-[16px] hover:text-bab">
               <CustomerServiceLine color="none" bgColor="none" size={16} />
               <p>고객센터</p>
             </Link>
