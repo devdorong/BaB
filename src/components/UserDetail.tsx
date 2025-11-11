0a1570988092af9e7417e0c7df700e8e5443a417
seastj <dev.seastj@gmail.com>
2025-11-11 14:20:32 +0900

    [docs] admin 스타일 수정

--- src/components/UserDetail.tsx
+++ src/components/UserDetail.tsx
@@ -3,17 +3,35 @@ import Modal from '@/ui/sdj/Modal';
 import { useModal } from '@/ui/sdj/ModalState';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const UserDetail = () => {
+interface UserDetailProps {
+  targetId: string;
+  nickname: string;
+}
+const UserDetail = ({ targetId, nickname }: UserDetailProps) => {
   const { id, user } = useParams();
   const navigate = useNavigate();
   const { closeModal, modal, openModal } = useModal();
 
-  const handleBlock = () => {
-    openModal('차단', '해당 사용자를 차단하시겠습니까?', '취소', '차단', async () => {
-      const { error } = await supabase.from('profile_blocks').insert({});
+  const handleBlock = async () => {
+    if (!user) return;
+
+    openModal('차단', `${nickname} 님을 차단하시겠습니까?`, '취소', '차단', async () => {
+      const { error } = await supabase.from('profile_blocks').insert({
+        profile_id: id,
+        blocked_profile_id: targetId,
+      });
+
       if (error) {
-        console.log(error);
+        if (error.code === '23505') {
+          console.log('이미 차단된 사용자입니다.');
+        } else {
+          console.error('차단 오류:', error.message);
+        }
+      } else {
+        console.log(`${nickname} 차단 완료`);
       }
+
+      closeModal();
     });
   };
 
@@ -29,7 +47,7 @@ const UserDetail = () => {
         채팅하기
       </button>
       <button
-        onClick={() => navigate('/')}
+        // onClick={() => navigate('/')}
         className="w-full px-4 py-2 text-left text-sm text-gray-700 rounded-sm hover:bg-gray-100"
       >
         차단하기
